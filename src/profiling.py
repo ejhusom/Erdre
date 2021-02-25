7887d6371aa093b1f51516d802a8a0500c2a2d9d
ejhusom <ejlgkvam@online.no>
2021-02-25 11:19:23 +0100

    Add profiling to the dvc pipeline

--- src/profiling.py
+++ src/profiling.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from pandas_profiling import ProfileReport
-from config import DATA_PATH
+from config import DATA_PATH_RAW, PROFILING_PATH
 
 
 # Set path to parent directory where the .csv files are located.
@@ -15,14 +15,17 @@ df = pd.DataFrame()
 
 # Read and append the first 9 .csv files.
 for i in range(1,10):
-    tmp = pd.read_csv(DATA_PATH / "experiment_0" + str(i) + ".csv")
+    tmp = pd.read_csv(DATA_PATH_RAW / ("experiment_0" + str(i) + ".csv"))
     df = df.append(tmp)
 
 #### End of read data section ####
 
 # Generate report.
-profile = ProfileReport(df, title="Profiling Analysis", config_file="myconfig.yaml", lazy=False)
+profile = ProfileReport(df, title="Profiling Analysis", config_file="src/myconfig.yaml", lazy=False)
+
+# Create folder for profiling report
+PROFILING_PATH.mkdir(parents=True, exist_ok=True)
 
 # Save report to html.
-profile.to_file("profile_report.html")
+profile.to_file(PROFILING_PATH / "profile_report.html")
 
