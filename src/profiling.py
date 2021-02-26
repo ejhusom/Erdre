24d6a830aae7b4510da66a911be1a9e331c355ff
ejhusom <ejlgkvam@online.no>
2021-02-26 11:34:20 +0100

    Make profiling script accept raw data from subfolders

--- src/profiling.py
+++ src/profiling.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from pandas_profiling import ProfileReport
+import yaml
+
 from config import DATA_PATH_RAW, PROFILING_PATH
 
 
@@ -13,9 +15,20 @@ from config import DATA_PATH_RAW, PROFILING_PATH
 # Define an empty data frame.
 df = pd.DataFrame()
 
+# Load parameters for finding raw files
+params = yaml.safe_load(open("params.yaml"))["featurize"]
+
+raw_subfolder = params["raw_subfolder"]
+"""Subfolder of 'assets/data/raw', in where to look for data."""
+
+dir_path = str(DATA_PATH_RAW)
+
+if raw_subfolder != None:
+    dir_path += "/" + raw_subfolder
+
 # Read and append the first 9 .csv files.
 for i in range(1,10):
-    tmp = pd.read_csv(DATA_PATH_RAW / ("experiment_0" + str(i) + ".csv"))
+    tmp = pd.read_csv(dir_path + "/" + "experiment_0" + str(i) + ".csv")
     df = df.append(tmp)
 
 #### End of read data section ####
