f3a8ae89a9e259ca00411c1c6dcf208969620f36
ejhusom <ejlgkvam@online.no>
2021-07-07 15:58:03 +0200

    Initial commit of script for segmentation

--- src/model.py
+++ src/model.py
@@ -56,11 +56,11 @@ def cnn(input_x, input_y,
         activation="elu", name="conv1d_1"))
     model.add(layers.Conv1D(filters=64, kernel_size=kernel_size,
         activation="relu", name="conv1d_2"))
-    # model.add(layers.MaxPooling1D(pool_size=4, name="pool_1"))
-    # model.add(layers.Conv1D(filters=32, kernel_size=kernel_size,
-    #     activation="relu", name="conv1d_3"))
-    # model.add(layers.Conv1D(filters=32, kernel_size=kernel_size,
-    #     activation="relu", name="conv1d_4"))
+    model.add(layers.MaxPooling1D(pool_size=4, name="pool_1"))
+    model.add(layers.Conv1D(filters=32, kernel_size=kernel_size,
+        activation="relu", name="conv1d_3"))
+    model.add(layers.Conv1D(filters=32, kernel_size=kernel_size,
+        activation="relu", name="conv1d_4"))
     model.add(layers.Dropout(rate=0.1))
     model.add(layers.Flatten(name="flatten"))
     model.add(layers.Dense(128, activation="relu", name="dense_1"))
---
+++ src/segmentation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Segmentation of time-series data.
+
+Author:   
+    Erik Johannes Husom
+
+Created:  
+    2021-07-07
+
+"""
+
+
+
