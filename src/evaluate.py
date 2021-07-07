fdd5af0eb08814a477431ccbf5600ddeba3f048f
ejhusom <ejlgkvam@online.no>
2021-07-07 12:17:29 +0200

    Fix typo

--- src/evaluate.py
+++ src/evaluate.py
@@ -172,7 +172,7 @@ def evaluate(model_filepath, train_filepath, test_filepath, calibrate_filepath):
     plot_prediction(y_test, y_pred, inputs=X_test, info="(R2: {})".format(r2))
 
     # Only plot predicted sequences if the output samples are sequences.
-    if len(y_shape) > 1 and y.shape[1] > 1:
+    if len(y_test.shape) > 1 and y_test.shape[1] > 1:
         plot_sequence_predictions(y_test, y_pred)
 
     with open(METRICS_FILE_PATH, "w") as f:
